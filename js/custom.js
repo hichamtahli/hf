16fe874ac82998a75c34f1848e3ab2ee2eb9ae63
hicham tahli <hicham.tahli@gmail.com>
2017-10-12 11:56:41 +0100

    Header

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+	/* NAVIGATION */
+	$('.popular').hover(
+	  function() {
+	    $('.nav1').slideDown();
+	  }, function() {
+	    $('.nav1').hide();
+	  }
+	);
+	/* END NAVIGATION */
+});	
